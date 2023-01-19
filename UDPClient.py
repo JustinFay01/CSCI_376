7238891b3d699dd8aad50c7aacefcd4ac9d7b41c
JustinFay01 <JustinFay@hope.edu>
2023-01-19 15:09:30 -0500

    Adding TCP server file as well as unlimited input UDP client

--- UDPClient.py
+++ UDPClient.py
@@ -2,8 +2,22 @@ from socket import *
 serverName = '192.168.1.34'
 serverPort = 1200
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
+
+def modifyMessage(message):
+	if(message == "bye"):
+		print("connection terminated")
+		clientSocket.close()
+	else:
+		clientSocket.sendto(message.encode(), (serverName, serverPort))
+		modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+		print(modifiedMessage.decode())
+
+print("Input lowercase sentence: ")
+
+while True:
+   modifyMessage(input())
+
+
+  
+
+
