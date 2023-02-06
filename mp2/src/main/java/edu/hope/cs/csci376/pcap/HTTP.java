e49615bde33919e68f8fb9bc47febef9ea0166ea
Justin Fay <justin.fay@hope.edu>
2023-02-06 13:48:44 -0500

    Tried to debug compression and added some comments

--- mp2/src/main/java/edu/hope/cs/csci376/pcap/HTTP.java
+++ mp2/src/main/java/edu/hope/cs/csci376/pcap/HTTP.java
@@ -2,6 +2,7 @@ package edu.hope.cs.csci376.pcap;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
 
 public class HTTP {
@@ -16,46 +17,55 @@ public class HTTP {
 
     }
 
-    // Looking for 47 45 54
-    // if string is empty and the first is new line print paylaod
     public void print() {
         System.out.println("--- HTTP ---");
         boolean gzip = false;
         String line = "";
         int encodeIndex = 0;
-        for (int i = 0; i < packet.length-1; i++) {
-            if ((char) packet[i] == '\r') {//Catch the end of a line
-                System.out.print(line + "\\r\\n"); //Print out the line of string
-                if(gzip && line.length() == 1){
-                    System.out.println("Need to encode");
+
+        for (int i = 0; i < packet.length; i++) {
+            if ((char) packet[i] == '\r') {// Catch the end of a line
+                System.out.print(line + "\\r\\n"); // Print out the line of string
+                if (gzip && line.length() == 1) {//We have reached the compressed payload
                     encodeIndex = i;
                     break;
                 }
-                line = ""; //reset the line                
+                line = ""; // reset the line
             } else {
-                line += String.valueOf((char) packet[i]); //Continue adding Characters to line until new line
-                if(line.contains("Content-Encoding: gzip"))//Check for encoding
+                line += String.valueOf((char) packet[i]); // Continue adding Characters to line until new line
+                if (line.contains("Content-Encoding: gzip"))// Check for encoding
                     gzip = true;
-                
-            }  
-        }
-        System.out.println("Loop broken");
-        byte[] toDecode = new byte[encodeIndex+1];
 
-        for(int i = encodeIndex, j = 0; i < packet.length; i++, j++){
-            toDecode[j] = packet[i];
-            System.out.print((char) toDecode[j]);
+            }
         }
 
-        if(gzip){
+        if (gzip) {
+            byte[] toDecode = new byte[packet.length-encodeIndex];
+            System.out.println();
+            for (int i = encodeIndex, j = 0; i < packet.length; i++, j++) {
+                toDecode[j] = packet[i];
+                System.out.print(toDecode[j] + " ");
+            }
+            System.out.println();
+            // InputStream by = new ByteArrayInputStream(toDecode);
+            
+            // try{
+            //     while(by.available() > 0){
+            //         System.out.print(by.read() + " ");
+            //     }
+            //     System.out.println();
+            //     gis = new GZIPInputStream(by);
+            // } catch (Exception e){
+            //     e.printStackTrace();
+            // }
             GZIPInputStream gis;
             byte[] decompressed;
-        try {
-            gis = new GZIPInputStream(new ByteArrayInputStream(toDecode));
-            decompressed = gis.readAllBytes();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            try {
+                gis = new GZIPInputStream(new ByteArrayInputStream(toDecode));
+                decompressed = gis.readAllBytes();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
