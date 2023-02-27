bc19cfd1f401f38fc46323ecf85e79e7bbb4cd88
JustinFay01 <JustinFay@hope.edu>
2023-02-26 19:31:38 -0500

    Cleaning to publish branch

--- mp2/src/main/java/edu/hope/cs/csci376/pcap/UDPPacket.java
+++ mp2/src/main/java/edu/hope/cs/csci376/pcap/UDPPacket.java
@@ -1,11 +1,27 @@
 package edu.hope.cs.csci376.pcap;
 
-public class UDPPacket {
+public class UDPPacket extends TransportLayer{
 
-    public UDPPacket(byte[] payload) {
+    public byte[] packet;
+
+    public UDPPacket(byte[] packet) {
+        super(packet);
+        this.packet = packet;
     }
 
     public void print() {
+        super.print();
+        System.out.println(packet.length);
+        System.out.println("   Length: " + getLength());
+    }
+
+    public int getLength(){
+        int len = (packet[34] & 0xFF) * 256 + (packet[35] & 0xFF); 
+        return len;
+    }
+
+    public int getCheckSum(){
+        return 0;
     }
     
 }
