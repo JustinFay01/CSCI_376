71d5f246125c1ab58e46a84af4a83de8ff9bb4ef
Justin Fay <justin.fay@hope.edu>
2023-02-26 20:58:40 -0500

    Optimized print flags

--- mp2/src/main/java/edu/hope/cs/csci376/pcap/TCPPacket.java
+++ mp2/src/main/java/edu/hope/cs/csci376/pcap/TCPPacket.java
@@ -45,41 +45,28 @@ public class TCPPacket extends TransportLayer {
         return ((packet[12] & 0xFF) / 4);
     }
 
+    public String getFormat(int i){
+	if(i == 0) return "      %1s... .... %s\n";
+	else if(i == 1) return "      .%1s.. .... %s\n";
+	else if(i == 2) return "      ..%1s. .... %s\n";
+	else if(i == 3) return "      ...%1s .... %s\n";
+	else if(i == 4) return "      .... %1s... %s\n";
+	else if(i == 5) return "      .... .%1s.. %s\n";
+	else if(i == 6) return "      .... ..%1s. %s\n";
+	else if(i == 7) return "      .... .... %s\n";
+	return "";
+    }
+
     //Take flags from specific byte
     public void printFlags() {
         String flags = String.format("%8s", Integer.toBinaryString(packet[13])).replace(' ', '0');
-        System.out.printf("    %1s... .... %s\n", // Window Reduced
-                flags.substring(0, 1), flags.substring(0, 1).equals("0")
-                        ? "Congestion Window Reduced: Not set"
-                        : "congestion Window Reduced: Set");
-        System.out.printf("    .%1s.. .... %s\n", // ECN-Echo
-                flags.substring(1, 2), flags.substring(1, 2).equals("0")
-                        ? "ECN-Echo: Not set"
-                        : "ECN-Echo: set");
-        System.out.printf("    ..%1s. .... %s\n", // Urgent
-                flags.substring(2, 3), flags.substring(2, 3).equals("0")
-                        ? "Urgent: Not set"
-                        : "Urgent: set");
-        System.out.printf("    ...%1s .... %s\n", // Ackowledgment
-                flags.substring(3, 4), flags.substring(3, 4).equals("0")
-                        ? "Acknowledgement: Not set"
-                        : "Acknowledgement: set");
-        System.out.printf("    .... %1s... %s\n", // Push
-                flags.substring(4, 5), flags.substring(4, 5).equals("0")
-                        ? "Push: Not Set"
-                        : "Push: set");
-        System.out.printf("    .... .%1s.. %s\n", // Reset
-                flags.substring(5, 6), flags.substring(5, 6).equals("0")
-                        ? "Reset: Not set"
-                        : "Reset: set");
-        System.out.printf("    .... ..%1s. %s\n", // SYN
-                flags.substring(6, 7), flags.substring(6, 7).equals("0")
-                        ? "SYN: Not Set"
-                        : "SYN: Set");
-        System.out.printf("    .... ...%1s %s\n", // FIN
-                flags.substring(7, 8), flags.substring(7, 8).equals("0")
-                        ? "FIN: Not set"
-                        : "FIN: set");
+        String[] flagOptions = {"Congestion Window Reduced", "ECN-Echo", "Urgent", "Acknowledgement", "Push", "SYN", "FIN" };
+
+	for(int i = 0; i + 1 < flags.length(); i++){
+                System.out.printf(getFormat(i), flags.substring(i, i+1), flags.substring(i, i+1).equals("0")
+                	? flagOptions[i] + ": Not set"
+                	: flagOptions[i] + ": Set");
+        }
     }
 
     public int getWindowSize(){
