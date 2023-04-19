25c7cfdaf67c59129e247c19e2863b4c3735aa32
JustinFay01 <JustinFay@hope.edu>
2023-04-18 23:00:17 -0400

    Added get Type, Found error with formatting of get source

--- mp5/src/main/java/edu/hope/cs/csci376/pcap/DataLinkLayer.java
+++ mp5/src/main/java/edu/hope/cs/csci376/pcap/DataLinkLayer.java
@@ -29,8 +29,9 @@ public class DataLinkLayer {
 
     public void print() {
         System.out.println("--- Ethernet ---");
-        System.out.println(getDestination());
-        System.out.println(getSource());
+        System.out.println("Destination: " + getDestination() + "\n"
+                + "Source: " + getSource() + "\n"
+                + "Type: " + getType());
     }
 
     /*
@@ -39,7 +40,7 @@ public class DataLinkLayer {
      * It is & with 0xFF to gain the first hex values (Changes ffffff --> ff)
      */
     public String getDestination() {
-        String destAddress = "Destination: ";
+        String destAddress = "";
         for (int i = 0; i <= 5; i++) {
             destAddress += Integer.toHexString(packet[i] & 0xFF);
             if (i != 5)
@@ -54,12 +55,32 @@ public class DataLinkLayer {
      * It is & with 0xFF to gain the first hex values (Changes ffffff --> ff)
      */
     public String getSource() {
-        String srcAddress = "Source: ";
+        String srcAddress = "";
         for (int i = 6; i <= 11; i++) {
             srcAddress += Integer.toHexString(packet[i] & 0xFF);
             if (i != 11)
                 srcAddress += ":";
         }
         return srcAddress;
+    }
+
+    public String getType() {
+        int type = (packet[12] & 0xFF) * 256 + (packet[13] & 0xFF);
+        switch (type) {
+            case TYPE_ARP:
+                return "0x0806";
+            case TYPE_RARP:
+                return "0x8035";
+            case TYPE_IPv4:
+                return "0x0800";
+            case TYPE_IPv6:
+                return "0x86DD";
+            case TYPE_LLDP:
+                return "0x88cc";
+            case TYPE_BSN:
+                return "0x8942";
+            default:
+                return "0x" + Integer.toHexString(type);
+        }
     }
 }
