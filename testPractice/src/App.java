1870b2441fd41a4103a039e2df3ce0601833b203
Justin Fay <justin.fay@hope.edu>
2023-05-02 23:51:32 -0400

    Commented out tests

--- testPractice/src/App.java
+++ testPractice/src/App.java
@@ -1,18 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        IPAddress ip = new IPAddress("192.168.0.10", "255.255.255.0");
-        ip.printInfo();
+        // IPAddress ip = new IPAddress("192.168.0.10", "255.255.255.0");
+        // ip.printInfo();
+
+        // printSeperator();
+        // ip = new IPAddress("209.140.209.140", "255.255.255.0");
+        // ip.printInfo();
+
+        // printSeperator();
+        // ip = new IPAddress("192.168.1.20", "255.255.240.0");
+        // ip.printInfo();
 
         printSeperator();
-        ip = new IPAddress("209.140.209.140", "255.255.255.0");
-        ip.printInfo();
-
-        printSeperator();
-        ip = new IPAddress("192.168.1.20", "255.255.240.0");
-        ip.printInfo();
-
-        printSeperator();
-        ip = new IPAddress("24.1.1.34", "255.255.255.248");
+        IPAddress ip = new IPAddress("209.140.209.25", "255.255.255.224");
         ip.printInfo();
     }
 
